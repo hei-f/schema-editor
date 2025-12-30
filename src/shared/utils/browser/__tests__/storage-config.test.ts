076736e2ab346626fd74eb2c9e5266ac0a8aa0a3
youling <liyoujun.lyj@digital-engine.com>
2025-12-30 19:12:32 +0800

    test: 修复 storage-config.test.ts 中的 enableDebugLog 引用

--- src/shared/utils/browser/__tests__/storage-config.test.ts
+++ src/shared/utils/browser/__tests__/storage-config.test.ts
@@ -8,7 +8,6 @@ describe('storage-config', () => {
         'attributeName',
         'drawerWidth',
         'autoParseString',
-        'enableDebugLog',
         'highlightColor',
         'maxFavoritesCount',
         'draftRetentionDays',
